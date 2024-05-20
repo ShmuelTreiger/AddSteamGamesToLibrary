f47a467379f32b0de77aa9d17ac8330fa05b3e92
ShmuelTreiger <shmueltreiger@gmail.com>
2024-05-20 15:38:59 -0700

    Implemented isort

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 
 import credentials
 
