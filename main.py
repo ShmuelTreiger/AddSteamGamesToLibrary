49d3d08e47288fd6065b9210b915169459587f57
ShmuelTreiger <shmueltreiger@gmail.com>
2024-05-20 02:27:56 -0700

    Add steam login

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import credentials
+
+steam_url = "https://www.steampowered.com"
 
 # Open browser
 driver = webdriver.Chrome()
+
+# Navigate to Steam
+driver.get(steam_url)
+
+# Navigate to login page
+e = driver.find_element(by=By.LINK_TEXT, value="login")
+e.click()
+
+# Login
+e = driver.find_element(by=By.CLASS_NAME, value="_2eKVn6g5Yysx9JmutQe7WV")
+e.send_keys(credentials.username)
+e = driver.find_element(by=By.XPATH, value="//*[@id=\"responsive_page_template_content\"]/div[3]/div[1]/div/div/div/div[2]/div/form/div[2]/input")
+e.send_keys(credentials.password)
+e.send_keys(Keys.ENTER)
 
 driver.close()
